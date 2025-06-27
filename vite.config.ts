e74728e446a87e6ca798d8ac69404831907507cf
Rahim <rahimch439@gmail.com>
2025-06-28 02:21:11 +0500

    changed vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://pakistani-panorama-backend.onrender.com',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path,
