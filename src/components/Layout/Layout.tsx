33e73d1980611ac6a8c8b7ec7b4236865d381af6
Rahim <rahimch439@gmail.com>
2025-07-07 22:27:40 +0500

    added Analytics

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../../Pages/Navbar';
 import Footer from '../Footer/Footer';
-
+import { Analytics } from "@vercel/analytics/next"
 const Layout = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
       <main className="flex-grow">
+        <Analytics />
         <Outlet />
       </main>
       <Footer />
